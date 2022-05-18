3bdf78be5ce679f580ed12fb68d6391e086f8dac
Ethan Lee <34255728+eduguy@users.noreply.github.com>
2022-05-18 15:58:00 -0700

    Add files via upload

---
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Texavie Developer on 2022-05-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
